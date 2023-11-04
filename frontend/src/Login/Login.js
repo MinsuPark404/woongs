10e1e08bb8d4b23322df529f80b354b1f67435f2
PARKMINSU <parkms6490@gmail.com>
2023-11-04 14:20:25 +0900

    -park

--- frontend/src/Login/Login.js
+++ frontend/src/Login/Login.js
@@ -3,53 +3,68 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-import './Login.css'; 
+import './Login.css';
 
 function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [message, setMessage] = useState('');
-    const navigate  = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            console.log('뭐가  문제냐 ')
-            const response = await axios.post('http://localhost:5000/login', {
-                username,
-                password
-            });
-            navigate('/main');
-            setMessage(response.data.message);
-        } catch (error) {
-            console.error('Login error', error);
-            setMessage('An error occurred while logging in');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log('뭐가  문제냐 ');
+      const response = await axios.post(
+        'http://localhost:5000/api/admins/login',
+        {
+          // username,
+          // password
+
+          admin_email: username, // 백엔드에서 요구하는 키 값에 맞춰서 변경
+          admin_password: password,
         }
-    };
+      );
+      navigate('/main');
+      setMessage(response.data.message);
+    } catch (error) {
+      console.error('Login error', error);
+      setMessage('An error occurred while logging in');
+    }
+  };
 
-    return (
-        
-            <div className="login-wrapper">
-            <div className="login-container">
-                <form onSubmit={handleSubmit}>
-                    <div className="logo">logo</div>
+  return (
+    <div className="login-wrapper">
+      <div className="login-container">
+        <form onSubmit={handleSubmit}>
+          <div className="logo">logo</div>
 
-                    <input type="text" placeholder="ID" className="id-input" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <input type="password" placeholder="Password" className="pw-input" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input
+            type="text"
+            placeholder="ID"
+            className="id-input"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="pw-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-                    <div className="button-group">
-                        <button className="btn-login" type="submit">Login</button>
-                        <button className="btn-ask">Ask</button>
-                    </div>
-                </form>
-                {message && <p>{message}</p>}
-            </div>
-            
-        
-            </div>
-
-            
-    );
+          <div className="button-group">
+            <button className="btn-login" type="submit">
+              Login
+            </button>
+            <button className="btn-ask">Ask</button>
+          </div>
+        </form>
+        {message && <p>{message}</p>}
+      </div>
+    </div>
+  );
 }
 
 export default Login;
