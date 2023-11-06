294e5904619b2540daf956176d46345ec4a839f9
Jungwook96 <swsmas@naver.com>
2023-11-06 16:43:51 +0900

    -정욱

--- frontend/src/components/AdminC/PermissionManagement.js
+++ frontend/src/components/AdminC/PermissionManagement.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PermissionManagement = () => {
   return (
     <div>
-      <h2>권한 관리</h2>
+      <h2>권한 관리2</h2>
       {/* 권한 관리 관련 내용을 여기에 추가 */}
     </div>
   );
