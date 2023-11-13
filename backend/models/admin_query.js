676628f2a7d593cb591051ac2c6a6f8eb9c67320
PARKMINSU <parkms6490@gmail.com>
2023-11-13 18:08:58 +0900

    박민수

--- backend/models/admin_query.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  selectAdmin: 'select * from admins where id = ? ',
-  b: 'asassas',
-};
--- backend/note.js
+++
@@ -1,9 +0,0 @@
-// @관리자 목록 조회
-// @Endpoint POST /api/admins/list
-// @access admin_s
-const businessList = asyncHandler(async (req, res) => {
-  const [admins] = await db.query('SELECT * FROM cms_admins');
-  res.status(200).json(admins);
-});
-
-await adminModel.getAlladmins;
