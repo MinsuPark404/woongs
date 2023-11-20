794537094e699fec488d213c9dd832b3133f506c
hyunsol <hyunsoloh@gmail.com>
2023-11-20 15:36:36 +0900

    UPDATE : Domain function -HS

--- frontend/src/components/Domain/DomainList.js
+++ frontend/src/components/Domain/DomainList.js
@@ -6,44 +6,38 @@ import axios from '../../axios';
 const DomainList = () => {
   const [domains, setDomains] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
   useEffect(() => {
     console.log('도메인 리스트 조회');
-    setDomains([
-      {
-        url_idx: 1,
-        url_addr: 'www.kin1.com',
-        url_status: 'T',
-        business_idx: '1',
-        url_archived_at: '2023.01.01',
-      },
-      {
-        url_idx: 2,
-        url_addr: 'www.kin2.com',
-        url_status: 'T',
-        business_idx: '1',
-        url_archived_at: '2023.01.01',
-      },
-      {
-        url_idx: 3,
-        url_addr: 'www.kin3.com',
-        url_status: 'T',
-        business_idx: '1',
-        url_archived_at: '2023.01.01',
-      },
-    ]);
+    const fetchData = async () => {
+      const data = await axios.get('/api/domains');
+      // console.log(data.data);
+      setDomains(data.data);
+    };
+    fetchData();
   }, []);
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
-
-  const filterDomains = (domains) => {
-    return domains.filter((domain) =>
-      domain.url_addr.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
-
+  const filterDomains = (domains) => {
+    return domains
+      .filter((domain) =>
+        domain.url_addr.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+  };
+  const remainDate = (url_period) =>{
+    const today = new Date();
+    const expireDate = new Date(url_period);
+    const remainTime = expireDate.getTime() - today.getTime();
+    const remainDay = Math.floor(remainTime / (1000 * 60 * 60 * 24));
+    return remainDay;
+  }
   const filteredDomains = filterDomains(domains);
   return (
     <div>
@@ -66,6 +60,7 @@ const DomainList = () => {
                 <th>도메인 상태</th>
                 <th>도메인 소유자</th>
                 <th>도메인 만료일</th>
+                <th>도메인 남은 날자(Day)</th>
               </tr>
             </thead>
             <tbody>
@@ -73,14 +68,25 @@ const DomainList = () => {
                 <tr key={domains.url_idx}>
                   <td>{domains.url_addr}</td>
                   <td>{domains.url_status}</td>
-                  <td>{domains.business_idx}</td>
-                  <td>{domains.url_archived_at}</td>
+                  <td>{domains.business_name}</td>
+                  <td>{domains.url_period}</td>
+                  <td>{remainDate(domains.url_period)}</td>
                 </tr>
               ))}
             </tbody>
           </table>
         ) : (
           <p>검색 결과가 없습니다.</p>
+        )}
+        
+      </div>
+      <div className="pagination">
+        {[...Array(Math.ceil(domains.length / itemsPerPage)).keys()].map(
+          (page) => (
+            <button key={page + 1} onClick={() => handlePageChange(page + 1)}>
+              {page + 1}
+            </button>
+          )
         )}
       </div>
     </div>
